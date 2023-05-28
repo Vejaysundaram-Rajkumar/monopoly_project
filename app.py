e838da27816fb8f4cf79a6bc1b0fe45b9625f502
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2023-05-28 12:55:26 +0530

    website for manage

---
+++ app.py
@@ -0,0 +1,23 @@
+from numerize import numerize
+from flask import Flask, render_template, request, redirect, session, url_for
+import sqlite3
+import hashlib
+
+#Creating an object for the flask called app
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+#Connect with the customers database to store the user details
+def connect_db():
+    connection = sqlite3.connect('gamedetails.db')
+    return connection
+
+#rendering the landing or the home page
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
